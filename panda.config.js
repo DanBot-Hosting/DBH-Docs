f822236907fbfc910ec33629ea0df7f573c82367
Jon <66625200+Kyro3400@users.noreply.github.com>
2024-10-04 10:10:00 -0400

    Update and rename panda.config.js to panda.config.ts
    Make it match with the way the auth-fontend file looks

--- panda.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@pandacss/dev'
- 
-export default defineConfig({
-  preflight: true,
-  include: ['./src/**/*.{ts,tsx,js,jsx}', './pages/**/*.{ts,tsx,js,jsx}'],
-  exclude: [],
-  outdir: 'styled-system',
-  presets: ['@danbot-hosting/panda-preset']
-})
---
+++ panda.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "@pandacss/dev";
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+  // Whether to update the .gitignore file.
+  gitignore: true,
+  // Whether to minify the generated CSS.
+  minify: true,
+  // Shorten classnames
+  hash: true,
+  // Opt out of default styles config
+  presets: ["@danbot-hosting/panda-preset"],
+  // Eject all default presets, we only use our own
+  eject: true,
+
+  // Where to look for css declarations
+  include: [
+    "./pages/**/*.{js,jsx,ts,tsx,mdx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+
+  // Files to exclude
+  exclude: [],
+
+  // The output directory for your css system
+  outdir: "styles",
+
+  jsxFramework: "react",
+});
